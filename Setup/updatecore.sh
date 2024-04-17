994b0a84d434c963d139da331c6728032db1949b
markbm <markbm@outlook.com>
2024-04-17 16:39:21 +0800

    update files

--- Setup/updatecore.sh
+++ Setup/updatecore.sh
@@ -4,7 +4,7 @@ systemctl stop xray-mini@nontls > /dev/null 2>&1
 cd /usr/local/kaizenvpn/xray-mini;
 rm xray-mini;
 
-wget -q -O /usr/local/kaizenvpn/xray-mini "https://raw.githubusercontent.com/rewasu91/scvps/main/Resource/Core/xray-mini";
+wget -q -O /usr/local/kaizenvpn/xray-mini "https://raw.githubusercontent.com/sunbear88/scvps/main/Resource/Core/xray-mini";
 chmod +x /usr/local/kaizenvpn/xray-mini;
 
 systemctl enable xray-mini@tls;
